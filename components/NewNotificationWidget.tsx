5c73067697b9fa6c57635273618ded1d88722bdf
kelly basil <99088537+kellybasil185@users.noreply.github.com>
2025-05-25 14:06:22 +0000

    Updated NewNotificationWidget.tsx

--- components/NewNotificationWidget.tsx
+++ components/NewNotificationWidget.tsx
@@ -339,7 +339,7 @@ export default function NewNotificationWidget() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={[styles.widgetContainer, { backgroundColor: isDark ? '#6b2537' : '#969596' }]}>
+      <View style={[styles.widgetContainer, { backgroundColor: isDark ? '#4a1e29' : '#969596' }]}>
         <Text style={[styles.widgetHeaderTitle, { color: isDark ? '#FFFFFF' : '#000000' }]}>
           Notifications
         </Text>
