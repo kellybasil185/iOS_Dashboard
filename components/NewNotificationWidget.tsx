2c40b8a8a0346e6a375b6a88d0df310aee07adbe
kelly basil <99088537+kellybasil185@users.noreply.github.com>
2025-05-25 13:34:21 +0000

    Updated NewNotificationWidget.tsx

--- components/NewNotificationWidget.tsx
+++ components/NewNotificationWidget.tsx
@@ -269,7 +269,7 @@ export default function NewNotificationWidget() {
     <View
       style={[
         styles.widgetContainer,
-        { backgroundColor: isDark ? '#120016' : '#F0F0F0' },
+        { backgroundColor: isDark ? '#71289C' : '#BED496' },
       ]}
     >
       <Text
